67a0288c5b0011980e2eb3488d146508366767cc
taylor-green <taylor.green2804@gmail.com>
2023-03-20 18:14:14 -0400

    first commit, began code

---
+++ Develop/index.js
@@ -0,0 +1,18 @@
+// TODO: Include packages needed for this application
+const questioner = require('questioner');
+const fs = require('fs');
+const util = require('util');
+
+// TODO: Create an array of questions for user input
+const generateMarkdown = require('/util/generateMarkdown');
+const license = require('/utils/license').licesne;
+const questions = require('/utils/questions').questions;
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
