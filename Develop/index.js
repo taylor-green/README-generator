23572e5847130521193b106687ffe876c48097a2
taylor-green <taylor.green2804@gmail.com>
2023-03-23 17:49:06 -0400

    added in questions

--- Develop/index.js
+++ Develop/index.js
@@ -1,18 +1,70 @@
 // TODO: Include packages needed for this application
-const questioner = require('questioner');
+const inquirer = require('inquirer');
 const fs = require('fs');
-const util = require('util');
 
-// TODO: Create an array of questions for user input
-const generateMarkdown = require('/util/generateMarkdown');
-const license = require('/utils/license').licesne;
-const questions = require('/utils/questions').questions;
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
 
-// TODO: Create a function to initialize app
-function init() {}
+inquirer.prompt(
+    [
+        {
+            type: 'input',
+            message: 'What is the project title?',
+            name: 'title',
+            // makes sure user provided a value
+            validate: (value) => {if(value){return true} else{return 'Input required to continue'}}
 
-// Function call to initialize app
-init();
+        },
+        {
+            type: 'input',
+            message: 'What is the description of the app?',
+            name: 'description',
+            validate:  (value) => {if(value){return true} else{return 'Input required to continue'}}
+        },
+        {
+            type: 'input',
+            message: 'How do you install this app?',
+            name: 'install',
+            validate:  (value) => {if(value){return true} else{return 'Input required to continue'}}
+        },
+        {
+            type: 'input',
+            message: 'How do you use this app?',
+            name: 'usage',
+            validate:  (value) => {if(value){return true} else{return 'Input required to continue'}}
+
+        },
+        {
+            type:'input',
+            message: 'Did you have any credits?',
+            name: 'credits',
+            validate:  (value) => {if(value){return true} else{return 'Input required to continue'}}
+        },
+        {
+            type:'list',
+            message: 'What license(s) did you use?',
+            name: 'license',
+            choices: ('The MIT license', 'The GPL license', 'Apache license', 'GMU license', 'N/A'),
+            validate:  (value) => {if(value){return true} else{return 'Input required to continue'}}
+        },
+        {
+            type:'input',
+            message: 'GitHub username:',
+            name: 'git',
+            validate:  (value) => {if(value){return true} else{return 'Input required to continue'}}
+        },
+        {
+            type:'input',
+            message: 'Did you have any collaborators?',
+            name: 'credits',
+            validate:  (value) => {if(value){return true} else{return 'Input required to continue'}}
+        },
+    ]
+).then((
+    title,
+    description,
+    install,
+    usage,
+    credits,
+    license,
+    git
+    ) => )   
