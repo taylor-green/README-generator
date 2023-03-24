55772a68d8a6d7825e3e1d299ad16c1c73382979
taylor-green <taylor.green2804@gmail.com>
2023-03-24 14:57:44 -0400

    created fucntion to generate README file, refactored part of code

--- Develop/index.js
+++ Develop/index.js
@@ -1,70 +1,148 @@
-// TODO: Include packages needed for this application
+
 const inquirer = require('inquirer');
 const fs = require('fs');
 
-
-
-inquirer.prompt(
-    [
-        {
-            type: 'input',
-            message: 'What is the project title?',
-            name: 'title',
-            // makes sure user provided a value
-            validate: (value) => {if(value){return true} else{return 'Input required to continue'}}
-
-        },
-        {
-            type: 'input',
-            message: 'What is the description of the app?',
-            name: 'description',
-            validate:  (value) => {if(value){return true} else{return 'Input required to continue'}}
-        },
-        {
-            type: 'input',
-            message: 'How do you install this app?',
-            name: 'install',
-            validate:  (value) => {if(value){return true} else{return 'Input required to continue'}}
-        },
-        {
-            type: 'input',
-            message: 'How do you use this app?',
-            name: 'usage',
-            validate:  (value) => {if(value){return true} else{return 'Input required to continue'}}
-
-        },
-        {
-            type:'input',
-            message: 'Did you have any credits?',
-            name: 'credits',
-            validate:  (value) => {if(value){return true} else{return 'Input required to continue'}}
-        },
-        {
-            type:'list',
-            message: 'What license(s) did you use?',
-            name: 'license',
-            choices: ('The MIT license', 'The GPL license', 'Apache license', 'GMU license', 'N/A'),
-            validate:  (value) => {if(value){return true} else{return 'Input required to continue'}}
-        },
-        {
-            type:'input',
-            message: 'GitHub username:',
-            name: 'git',
-            validate:  (value) => {if(value){return true} else{return 'Input required to continue'}}
-        },
-        {
-            type:'input',
-            message: 'Did you have any collaborators?',
-            name: 'credits',
-            validate:  (value) => {if(value){return true} else{return 'Input required to continue'}}
-        },
-    ]
-).then((
+// Questions to appear in termninal
+inquirer.prompt([
+    {
+        type: 'input',
+        message: 'What is the project title?',
+        name: 'title',
+        validate: (value) => {
+            if (value) {
+                return true;
+            } else {
+                return 'Input required to continue';
+            }
+        }
+    },
+    {
+        type: 'input',
+        message: 'What is the description of the app?',
+        name: 'description',
+        validate: (value) => {
+            if (value) {
+                return true;
+            } else {
+                return 'Input required to continue';
+            }
+        }
+    },
+    {
+        type: 'input',
+        message: 'How do you install this app?',
+        name: 'install',
+        validate: (value) => {
+            if (value) {
+                return true;
+            } else {
+                return 'Input required to continue';
+            }
+        }
+    },
+    {
+        type: 'input',
+        message: 'How do you use this app?',
+        name: 'usage',
+        validate: (value) => {
+            if (value) {
+                return true;
+            } else {
+                return 'Input required to continue';
+            }
+        }
+    },
+    {
+        type: 'input',
+        message: 'Did you have any credits?',
+        name: 'credits',
+        validate: (value) => {
+            if (value) {
+                return true;
+            } else {
+                return 'Input required to continue';
+            }
+        }
+    },
+    {
+        type: 'list',
+        message: 'What license(s) did you use?',
+        name: 'license',
+        choices: ['The MIT license', 'The GPL license', 'Apache license', 'GMU license', 'N/A'],
+        validate: (value) => {
+            if (value) {
+                return true;
+            } else {
+                return 'Input required to continue';
+            }
+        }
+    },
+    {
+        type: 'input',
+        message: 'GitHub username:',
+        name: 'git',
+        validate: (value) => {
+            if (value) {
+                return true;
+            } else {
+                return 'Input required to continue';
+            }
+        }
+    },
+    {
+        type: 'input',
+        message: 'Did you have any collaborators?',
+        name: 'collaborators',
+        validate: (value) => {
+            if (value) {
+                return true;
+            } else {
+                return 'Input required to continue';
+            }
+        }
+    }
+]).then(({
     title,
     description,
     install,
     usage,
     credits,
     license,
-    git
-    ) => )   
+    git,
+    collaborators
+}) => {
+    // template to use
+    const template = `# ${title}
+
+# Description
+${description}
+
+# Installation
+${install}
+
+# Usage
+${usage}
+
+# Contribution
+${git}
+
+# Credits
+${credits}
+
+# License
+${license}
+
+# Collaborators
+${collaborators}`;
+
+    createNewFile(title, template);
+});
+// function to generate the new README
+function createNewFile(fileName, data) {
+    fs.writeFile(`./${fileName.toLowerCase().split(' ').join('')}.md`, data, (err) => {
+        if (err) {
+            console.log(err);
+        }
+        console.log('Your README has been created');
+    });
+}
--- README.md
+++ README.md
@@ -1,3 +1,7 @@
-# Professional README Generator Starter Code
+# README-Generator
+
+## Description
+This app was created to generate a basic README.md file by answering prompts in the terminal about an user created app
+
 
 [How to create a Professional README](https://coding-boot-camp.github.io/full-stack/github/professional-readme-guide)
